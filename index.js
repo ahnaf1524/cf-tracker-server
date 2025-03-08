b32a78f9776c882ebcbcd5e1b1d1f71c8d23e5bf
ahnaf1524 <ahnaf.tahmid.career@gmail.com>
2025-03-08 16:24:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
 const os = require('os');
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
